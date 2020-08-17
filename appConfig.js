b30bf4b7c95fbec6c2f08f412f68fa0f2db5c8af
Fatih Mehmet ARSLAN <iletisim@fmarslan.com>
2020-08-17 10:18:10 +0300

    Create appConfig.js

---
+++ appConfig.js
@@ -0,0 +1,7 @@
+var appConfiguration={
+  "backendUrl": "http://publication.ttk-journal.svc.cluster.sivas/api",
+  "fileServerUrl": "http://localhost:4002/api",
+  "ssoUrlAddress": "Red",
+  "language": "TR",
+  "pageStandart": "nope"
+}
